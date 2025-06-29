5b58d0bbd55d6f17f5bc5890cf7fc0c7fbdd3704
priyalramani <50806583+priyalramani@users.noreply.github.com>
2025-06-29 17:55:51 +0530

    Minor placeholder tweak

--- src/pages/MainAdmin/OrderAssembly.jsx
+++ src/pages/MainAdmin/OrderAssembly.jsx
@@ -6,11 +6,11 @@ const OrderAssembly = () => (
   <>
     <Sidebar />
     <Header />
-    <div className="item-sales-container orders-report-container">
+  <div className="item-sales-container orders-report-container">
       <div id="heading">
         <h2>Order Assembly</h2>
       </div>
-      <div style={{ padding: "20px", textAlign: "center" }}>Coming Soon...</div>
+      <div style={{ padding: "20px", textAlign: "center" }}>Coming Soon…</div>
     </div>
   </>
 );

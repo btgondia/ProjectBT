c923241b26a687ad05541a38f3bf280cd1ff7520
priyalramani <50806583+priyalramani@users.noreply.github.com>
2025-06-29 18:16:14 +0530

    Document Order Assembly placeholder

---
+++ src/pages/MainAdmin/OrderAssembly.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "../../components/Header";
+import Sidebar from "../../components/Sidebar";
+
+const OrderAssembly = () => (
+  <>
+    <Sidebar />
+    <Header />
+  <div className="item-sales-container orders-report-container">
+      <div id="heading">
+        <h2>Order Assembly</h2>
+      </div>
+      <div style={{ padding: "20px", textAlign: "center" }}>Coming Soon...</div>
+    </div>
+  </>
+);
+
+export default OrderAssembly;
